32a9c6ffc7c3eb44f0cf8224b910466dde730b2d
Yijiang Huang <yijiangh@mit.edu>
2019-08-13 03:25:36 +0200

    Add extra tcp transf to grasp_gen

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
 from .ss_pybullet.pybullet_tools.utils import *
+from .ss_pybullet.pybullet_tools.kuka_primitives import *
 from .ss_pybullet.motion import *
+from .utils.pick_primitives import *
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- utils/pick_primitives.py
+++ utils/pick_primitives.py
@@ -8,14 +8,14 @@ sys.path.append(os.path.join(directory, '../ss_pybullet'))
 
 from pybullet_tools.pr2_utils import get_top_grasps, get_side_grasps
 from pybullet_tools.utils import link_from_name, get_body_name, GraspInfo, Pose, \
-    INF, Point, approximate_as_prism, multiply, point_from_pose, unit_pose
+    INF, Point, approximate_as_prism, multiply, point_from_pose, unit_pose, invert
 from pybullet_tools.kuka_primitives import BodyGrasp
 
 GRASP_LENGTH = 0.
 MAX_GRASP_WIDTH = np.inf
 
 GRASP_INFO = {
-    # get_grasp_fun, approach_pose
+    # GraspInfo = namedtuple('GraspInfo', ['get_grasps', 'approach_pose'])
     'top': GraspInfo(lambda body: get_top_grasps(body, under=True, tool_pose=Pose(),
                                                  max_width=INF,  grasp_length=0),
                      Pose(0.1*Point(z=1))),
@@ -33,7 +33,7 @@ TOOL_FRAMES = {
 
 
 def get_bottom_grasps(body, under=False, tool_pose=Pose(), body_pose=unit_pose(),
-                   max_width=MAX_GRASP_WIDTH, grasp_length=GRASP_LENGTH):
+                      max_width=MAX_GRASP_WIDTH, grasp_length=GRASP_LENGTH):
     """used for eth_rfl_pick test"""
     center, (w, l, h) = approximate_as_prism(body, body_pose=body_pose)
     reflect_z = Pose(euler=[0, 0, 0])
@@ -54,14 +54,39 @@ def get_bottom_grasps(body, under=False, tool_pose=Pose(), body_pose=unit_pose()
     return grasps
 
 
-def get_grasp_gen(robot, grasp_name, tool_frame_name):
+def get_grasp_gen(robot, grasp_name, tool_frame_name, tool_from_tcp=None):
+    """Get a grasp generator.
+
+    Parameters
+    ----------
+    robot : type
+    grasp_name : str
+        name of the grasp class
+        only supports 'top', 'side', 'botttom' for now
+    tool_frame_name : str
+        e.g. 'ee_link'
+    tool_from_tcp : Pose
+        extra ee_mount_link to tcp transformation if any, defaults to None
+
+    Returns
+    -------
+    function handle
+        gen(pybullet body) : BodyGrasp
+
+    """
+    # GraspInfo : ['get_grasps', 'approach_pose'])
     grasp_info = GRASP_INFO[grasp_name]
     end_effector_link = link_from_name(robot, tool_frame_name)
 
     def gen(body):
         grasp_poses = grasp_info.get_grasps(body)
-        for grasp_pose in grasp_poses:
-            body_grasp = BodyGrasp(body, grasp_pose, grasp_info.approach_pose,
+        for tool_from_body in grasp_poses:
+            # approach_pose = grasp_from_approach
+            if tool_from_tcp:
+                tcp_from_body = multiply(invert(tool_from_tcp), tool_from_body)
+            else:
+                tcp_from_body = tool_from_body
+            body_grasp = BodyGrasp(body, tcp_from_body, grasp_info.approach_pose,
                                    robot, end_effector_link)
             yield (body_grasp,)
     return gen
